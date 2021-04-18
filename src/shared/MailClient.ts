42e737304cfb1ddfab9f7766beda8846f62ce5df
Raisess <raisechif@gmail.com>
2021-04-18 17:06:26 -0300

    implemented nodemailer adapter

---
+++ src/shared/MailClient.ts
@@ -0,0 +1,41 @@
+import { createTransport, Transporter } from "nodemailer";
+
+type Auth = {
+  user: string;
+  pass: string;
+};
+
+export default class MailClient {
+  private transport: Transporter;
+  private port: number = 587;
+  private secure: boolean = false;
+
+  private auth: Auth;
+
+  constructor(host: string, auth: Auth) {
+    this.auth = auth;
+
+    this.transport = createTransport({
+      host,
+      port: this.port,
+      secure: this.secure,
+      auth: this.auth,
+    });
+  }
+
+  public async sendMail(
+    receivers: Array<string>,
+    subject: string,
+    content: string,
+    html: boolean = false,
+  ): Promise<any> {
+    const info: any = await this.transport.sendMail({
+      from: this.auth.user,
+      to: receivers.length > 1 ? receivers.join(", ") : receivers[0],
+      subject: subject,
+      [html ? "html" : "text"]: content,
+    });
+
+    return info;
+  }
+}
